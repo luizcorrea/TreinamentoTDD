2572ded7d723424da128aef5c81b5bec72df28c7
Luiz Henrique C. Correa <luizhcorrea@gmail.com>
2016-01-30 15:58:05 -0200

    Criação da interface e do novo construtor com o repositorio

--- ExemploTDDComMoq/Business/Produto.cs
+++ ExemploTDDComMoq/Business/Produto.cs
@@ -4,10 +4,17 @@ namespace Business
 {
     public class Produto
     {
+        private IRepositorioProduto repositorio;
+
         public string Nome { get; set; }
 
         public Produto()
         {
+        }
+
+        public Produto(IRepositorioProduto repositorio)
+        {
+            this.repositorio = repositorio;
         }
 
         public bool Catalogar()
