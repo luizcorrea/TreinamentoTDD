5ba442cbd2bec6e8b7c9d4d76da6cc4f87be3ac8
Luiz Henrique C. Correa <luizhcorrea@gmail.com>
2016-01-30 15:20:44 -0200

    Criação do Método Catalogar
    Criação do método catalogar para rodar o teste e ve-lo falhar.

--- ExemploTDDComMoq/Business/Produto.cs
+++ ExemploTDDComMoq/Business/Produto.cs
@@ -1,9 +1,16 @@
-﻿namespace Business
+﻿using System;
+
+namespace Business
 {
     public class Produto
     {
         public Produto()
         {
+        }
+
+        public bool Catalogar()
+        {
+            throw new NotImplementedException();
         }
     }
 }
