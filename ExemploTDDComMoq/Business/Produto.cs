a0c0d3a11d1b7678546c766f761fbb64055569a0
Luiz Henrique C. Correa <luizhcorrea@gmail.com>
2016-01-30 15:38:53 -0200

    Alteração do método Catalogar para passar no novo cenário
    dentro do método Catalogar criamos uma validação para verificar se o
    nome o produto está preenchido, como solicita o cenário
    Deve_Retornar_False_Ao_Tentar_Catalogar_Um_Produto_Sem_Nome_Preenchido().
    Essa alteração fez o teste passar mas quebrou o primeiro teste.

--- ExemploTDDComMoq/Business/Produto.cs
+++ ExemploTDDComMoq/Business/Produto.cs
@@ -4,12 +4,19 @@ namespace Business
 {
     public class Produto
     {
+        public string Nome { get; set; }
+
         public Produto()
         {
         }
 
         public bool Catalogar()
         {
+            if(string.IsNullOrEmpty(this.Nome))
+            {
+                return false;
+            }
+
             return true;
         }
     }
