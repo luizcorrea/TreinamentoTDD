3d3ae91792d5702b37190da186a669aa14f31369
Luiz Henrique C. Correa <luizhcorrea@gmail.com>
2016-01-30 15:03:59 -0200

    Criação dos Projetos
    Criação dos projetos de teste e Business

---
+++ ExemploTDDComMoq/BusinessTest/ProdutoTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BusinessTest
+{
+    [TestClass]
+    public class ProdutoTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
